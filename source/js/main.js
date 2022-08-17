4bf67414f6e7c587fc435115c8020644c29f907e
tkornilova <kornilova.tat07@gmail.com>
2022-08-18 02:19:44 +0400

    add price and advertisement (html, css, js)

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
   const bodyWrapper = document.querySelector('.wrapper');
+
   const videoContainer = document.querySelector('.gym__video');
   const video = document.querySelector('iframe');
+
+  const durationList = document.querySelectorAll('.prices__duration p');
+  const priceLists = document.querySelectorAll('.prices__options ul');
 
   // Проверка JS
   if (bodyWrapper.classList.contains('wrapper-no-js')) {
@@ -28,4 +32,46 @@ window.addEventListener('DOMContentLoaded', () => {
   */
 
   videoContainer.addEventListener('click', removeVideoCover);
+
+  // Цены на абонементы
+  const changePriceCard = (duration) => {
+    duration.addEventListener('click', () => {
+      let index = Array.from(durationList).indexOf(duration);
+
+      durationList.forEach((item) => {
+        item.classList.remove('prices__duration--active');
+      });
+
+      duration.classList.add('prices__duration--active');
+
+      priceLists.forEach((list) => {
+        list.classList.remove('prices__list--active');
+      });
+
+      priceLists[index].classList.add('prices__list--active');
+    });
+  };
+
+  for (let i = 0; i < durationList.length; i++) {
+    durationList.forEach((duration) => {
+      duration.addEventListener('click', changePriceCard(duration));
+      duration.addEventListener('keydown', (evt) => {
+        if (evt.keyCode === 13 || evt.keyCode === 32) {
+          let index = Array.from(durationList).indexOf(duration);
+
+          durationList.forEach((item) => {
+            item.classList.remove('prices__duration--active');
+          });
+
+          duration.classList.add('prices__duration--active');
+
+          priceLists.forEach((list) => {
+            list.classList.remove('prices__list--active');
+          });
+
+          priceLists[index].classList.add('prices__list--active');
+        }
+      });
+    });
+  }
 });

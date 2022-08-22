7a9cf3b4a5286d390b5a9a8c5da483b32b130256
tkornilova <kornilova.tat07@gmail.com>
2022-08-22 12:04:26 +0400

    small fixes

--- source/js/main.js
+++ source/js/main.js
@@ -6,18 +6,35 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const videoContainer = document.querySelector('.gym__video');
   const video = document.querySelector('iframe');
+  const videoButton = document.querySelector('.gym__video button');
 
-  const durationList = document.querySelectorAll('.prices__duration p');
+  const durationList = document.querySelectorAll('.prices__duration > p');
   const priceLists = document.querySelectorAll('.prices__options ul');
 
+  const reviewsList = document.querySelectorAll('.reviews__item');
+
+  const form = document.querySelector('.form__wrapper form');
   const phoneInput = document.querySelector('.form__phone input');
+  const nameInput = document.querySelector('.form__name input');
+  const formButton = document.querySelector('.form__wrapper button');
 
   // Проверка JS
   if (bodyWrapper.classList.contains('wrapper-no-js')) {
     bodyWrapper.classList.remove('wrapper-no-js');
 
     videoContainer.classList.add('gym__video--nonactive');
+    videoContainer.removeAttribute('tabindex');
+    videoButton.style.display = 'initial';
     video.remove();
+
+    document.querySelector('.trainers__button-prev').removeAttribute('disabled');
+    document.querySelector('.trainers__button-next').removeAttribute('disabled');
+    document.querySelector('.reviews__button-prev').removeAttribute('disabled');
+    document.querySelector('.reviews__button-next').removeAttribute('disabled');
+
+    reviewsList.forEach((review) => {
+      review.style.margin = '0 20px 0 0';
+    });
 
     phoneInput.setAttribute('minlength', '18');
     phoneInput.setAttribute('maxlength', '18');
@@ -28,6 +45,7 @@ window.addEventListener('DOMContentLoaded', () => {
   const removeVideoCover = () => {
     if (videoContainer.classList.contains('gym__video--nonactive')) {
       videoContainer.classList.remove('gym__video--nonactive');
+      videoButton.style.display = 'none';
       videoContainer.insertAdjacentHTML('afterbegin', '<iframe src="https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1" title="YouTube video player" allow="autoplay" frameBorder="0" allowfullscreen></iframe>');
     }
   };
@@ -41,7 +59,7 @@ window.addEventListener('DOMContentLoaded', () => {
   video.addEventListener('ended', addVideoCover);
   */
 
-  videoContainer.addEventListener('click', removeVideoCover);
+  videoButton.addEventListener('click', removeVideoCover);
 
   // Цены на абонементы
   const changePriceCard = (duration) => {
@@ -70,10 +88,10 @@ window.addEventListener('DOMContentLoaded', () => {
           let index = Array.from(durationList).indexOf(duration);
 
           durationList.forEach((item) => {
-            item.classList.remove('prices__duration--active');
+            item.classList.remove('prices__duration-item--active');
           });
 
-          duration.classList.add('prices__duration--active');
+          duration.classList.add('prices__duration-item--active');
 
           priceLists.forEach((list) => {
             list.classList.remove('prices__list--active');
@@ -124,5 +142,16 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Mask
   maskPhone('.form__phone input');
+
+  // Submit form
+
+  formButton.addEventListener('click', () => {
+    if (nameInput.value !== '' && phoneInput.value !== '' & phoneInput.value.length === 18) {
+      localStorage.setItem('name', nameInput.value);
+      localStorage.setItem('phone', phoneInput.value);
+    } else {
+      form.reportValidity();
+    }
+  });
 
 });
